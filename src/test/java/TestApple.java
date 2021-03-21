a232abbd7b94d6ee6515017dd8c6b7d03a61518c
Elezium <elezium@thelostcloud.com>
2021-03-21 13:29:25 -0400

    testo

---
+++ src/test/java/TestApple.java
@@ -0,0 +1,46 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class TestApple {
+
+    public String baseUrl = "https://www.apple.com";
+//    String driverPath = "C:\\geckodriver.exe";
+    public WebDriver driver ;
+
+
+    @Test
+    public void verifyHomepageTitle() throws IOException {
+
+        DesiredCapabilities dr = new DesiredCapabilities();
+        //specify the browser
+        dr.setBrowserName("chrome");
+        //specify the environment
+        dr.setPlatform(Platform.LINUX);
+
+        System.out.println("launching firefox browser");
+        //        System.setProperty("webdriver.gecko.driver", driverPath);
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dr);
+        driver.get(baseUrl);
+        String expectedTitle = "Apple";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+        TakesScreenshot screenie = (TakesScreenshot)driver;
+        File screen1 =  screenie.getScreenshotAs(OutputType.FILE);
+        File DestFile = new File("/tmp/apple.png");
+        FileUtils.copyFile(screen1, DestFile);
+        driver.close();
+    }
+
+
+}
--- src/test/java/TestMicrosoft.java
+++ src/test/java/TestMicrosoft.java
@@ -1,25 +1,20 @@
-
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import org.testng.Assert;
-import org.testng.annotations.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.TakesScreenshot;
-import org.testng.asserts.IAssert;
-
-
 import java.net.URL;
 
-public class TestOne {
+public class TestMicrosoft {
 
-    public String baseUrl = "http://demo.guru99.com/test/newtours/";
+    public String baseUrl = "https://www.microsoft.com";
 //    String driverPath = "C:\\geckodriver.exe";
     public WebDriver driver ;
 
@@ -28,22 +23,21 @@ public class TestOne {
     public void verifyHomepageTitle() throws IOException {
 
         DesiredCapabilities dr = new DesiredCapabilities();
-//specify the browser
+        //specify the browser
         dr.setBrowserName("chrome");
-//specify the environment
+        //specify the environment
         dr.setPlatform(Platform.LINUX);
 
         System.out.println("launching firefox browser");
-//        System.setProperty("webdriver.gecko.driver", driverPath);
-//        driver = new RemoteWebDriver(new URL("http://docker-20:4444/wd/hub"), dr);
+        //        System.setProperty("webdriver.gecko.driver", driverPath);
         driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dr);
         driver.get(baseUrl);
-        String expectedTitle = "Welcome: Mercury Tours";
+        String expectedTitle = "Micro.."; // Fail on purpose
         String actualTitle = driver.getTitle();
         Assert.assertEquals(actualTitle, expectedTitle);
         TakesScreenshot screenie = (TakesScreenshot)driver;
         File screen1 =  screenie.getScreenshotAs(OutputType.FILE);
-        File DestFile = new File("/tmp/test.png");
+        File DestFile = new File("/tmp/microsoft.png");
         FileUtils.copyFile(screen1, DestFile);
         driver.close();
     }
